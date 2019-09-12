6c3283ad143beacbaae92e91038efec25b6be43a
ptr <kptr92@gmail.com>
2019-09-12 14:12:32 +0200

    start

---
+++ app/src/main/java/com/ptr/currency/App.kt
@@ -0,0 +1,5 @@
+package com.ptr.currency
+
+import android.app.Application
+
+class App : Application()
--- app/src/main/java/com/ptr/currency/ui/MainActivity.kt
+++ app/src/main/java/com/ptr/currency/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.ptr.currency
+package com.ptr.currency.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ptr.currency.R
 
 class MainActivity : AppCompatActivity() {
 
